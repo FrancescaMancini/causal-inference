ce7f52a721f63e37245b71523ad8e40974566f66
FrancescaMancini <framan@ceh.ac.uk>
2026-02-11 15:57:42 +0000

    Clean and merge data

--- 01_BeeWalk_data.R
+++ 01_BeeWalk_data.R
@@ -1,11 +1,70 @@
 #### BeeWalk site coordinates
+library(tidyverse)
 
-
-beewalk <- read.csv("BeeWalk data 2008-24 09012025.csv", header = TRUE, stringsAsFactors = FALSE)
+beewalk <- read.csv("./Data/BeeWalk data 2008-24 09012025.csv", header = TRUE, stringsAsFactors = FALSE)
 
 beewalk_sites <- unique(beewalk[c("GridReference", "Year", "Transect.lat", "Transect.long", "Precision")])
 
-write.csv(beewalk_sites, "BeeWalk_sites.csv", row.names = FALSE)
+write.csv(beewalk_sites, "./data/BeeWalk_sites.csv", row.names = FALSE)
 
 summary(beewalk$Precision)
 table(beewalk$Precision)
+
+
+# filters and checks
+
+beewalk <- beewalk %>%
+  filter(Year > 2013) %>%
+  filter(Precision <= 1000)
+
+nyear_by_site <- beewalk %>%
+  group_by(GridReference) %>%
+  summarise(n_years = n_distinct(Year))
+
+
+hist(nyear_by_site$n_years)
+# exclude all sites that have only been visited in < 3 years
+
+beewalk_filt <- beewalk %>%
+  group_by(GridReference) %>%
+  filter(n_distinct(Year) >= 3) %>%
+  ungroup()
+
+# check if any site name maps to more than one grid reference
+
+duplicated_sites <- beewalk_filt %>%
+  distinct(SiteName, GridReference) %>%
+  add_count(SiteName, name = "n_gridrefs_for_site") %>%
+  filter(n_gridrefs_for_site > 1)
+
+# 12 site names map to two different grid references
+# grid reference are very similar, so maybe only due to small changes in the start of the transect
+
+# Check if any grid reference maps to more than one site name
+
+duplicated_grefs <- beewalk_filt %>%
+  distinct(GridReference, SiteName) %>%
+  add_count(GridReference, name = "n_sites_for_gridref") %>%
+  filter(n_sites_for_gridref > 1)
+
+# and 70 grid references map to 2 site names
+# most of these are typos or sites being renamed something slightly different
+
+# not sure how to fix these...
+# possibly best to use grid references to identify transects as they seem to be more reliable
+# for now remove the 12 sites that are associated with more than 1 grid ref
+
+beewalk_filt <- beewalk_filt %>%
+  filter(!(GridReference %in% duplicated_sites$GridReference))
+
+
+# spatial distribution of transects through time
+
+ggplot(beewalk_filt,
+       aes(Transect.long, Transect.lat)) +
+  geom_point() +
+  facet_wrap(~Year) +
+  coord_equal() +
+  theme_bw()
+
+saveRDS(beewalk_filt, "./Data/beewalk_filtered.rds")
---
+++ 02_drivers_data.R
@@ -0,0 +1,151 @@
+## Exploring driver data
+library(tidyverse)
+
+
+drivers <- readRDS("./Data/beewalk_environmental_drivers_centroids.rds") 
+#10173
+
+# explore spatial variation in land cover
+
+drivers_long <- drivers %>%
+  select(site_id, BNG_east, BNG_north,
+         lcm_2024_Deciduous_woodland_pct:lcm_2024_Saltmarsh_pct) %>%
+  pivot_longer(starts_with("lcm_2024"), 
+               names_to = "landcover_type",
+               values_to = "value") %>%
+  filter(landcover_type %in% c("lcm_2024_Arable_pct",
+                               "lcm_2024_Urban_pct",
+                               "lcm_2024_Deciduous_woodland_pct",
+                               "lcm_2024_Coniferous_woodland_pct" ))
+
+
+
+ggplot(drivers_long, aes(BNG_east, BNG_north, colour = value)) +
+  geom_point(size = 3) +
+  scale_colour_viridis_c() +
+  facet_grid(~landcover_type) +
+  coord_equal() +
+  theme_bw()
+
+# what about pesticide load?
+# no temporal signal
+
+ggplot(drivers, aes(Year, pesticide_total_load, group = site_id, colour = site_id)) +
+  geom_line(alpha = 0.4) +
+  theme_bw() +
+  theme(legend.position = "none")
+
+ggplot(drivers, aes(BNG_east, BNG_north, colour = pesticide_total_load)) +
+  geom_point(size = 3) +
+  scale_colour_viridis_c() +
+  facet_grid(~Year) +
+  coord_equal() +
+  theme_bw()
+
+
+
+# how about temperature
+# this is the most promising
+# maybe we can derive some temperature anomaly metrics
+
+ggplot(drivers, aes(Year, temp_mean, group = site_id, colour = site_id)) +
+  geom_line(alpha = 0.4) +
+  theme_bw() +
+  theme(legend.position = "none") +
+  xlim(2010,2024)
+
+ggplot(drivers %>%
+         filter(Year > 2009), 
+       aes(BNG_east, BNG_north, colour = temp_mean)) +
+  geom_point(size = 3) +
+  scale_colour_viridis_c() +
+  facet_grid(~Year) +
+  coord_equal() +
+  theme_bw()
+
+
+# calculate landcover change
+
+
+# Step 1: Keep one row per site (land cover is same across years)
+sites <- drivers %>%
+  distinct(site_id, .keep_all = TRUE)
+
+# Step 2: Reshape land-cover columns into long format
+lcm_long <- sites %>%
+  select(site_id, BNG_east, BNG_north,
+         lcm_2024_Deciduous_woodland_pct, lcm_1990_Deciduous_woodland_pct,
+         lcm_2024_Coniferous_woodland_pct, lcm_1990_Coniferous_woodland_pct,
+         lcm_2024_Arable_pct, lcm_1990_Arable_pct,
+         lcm_2024_Urban_pct, lcm_1990_Urban_pct,
+         lcm_2024_Suburban_pct, lcm_1990_Suburban_pct) %>%
+  mutate(lcm_2024_forest = lcm_2024_Deciduous_woodland_pct + lcm_2024_Coniferous_woodland_pct,
+         lcm_1990_forest = lcm_1990_Deciduous_woodland_pct + lcm_1990_Coniferous_woodland_pct,
+         lcm_2024_urban = lcm_2024_Urban_pct + lcm_2024_Suburban_pct,
+         lcm_1990_urban = lcm_1990_Urban_pct, lcm_1990_Suburban_pct) %>%
+  select(site_id, BNG_east, BNG_north, lcm_2024_forest, lcm_1990_forest,
+         lcm_2024_urban, lcm_1990_urban, lcm_2024_Arable_pct, lcm_1990_Arable_pct) %>%
+  pivot_longer(
+    cols = matches("^lcm_"),
+    names_to = c("prefix", "year", "landcover_type"),
+    names_sep = "_",
+    values_to = "value"
+  ) %>%
+  select(site_id, BNG_east, BNG_north, year, landcover_type, value)
+
+
+
+
+# Step 3: Reshape to wide again to get year_1990 and year_2024 columns
+lcm_change <- lcm_long %>%
+  pivot_wider(
+    names_from = year,
+    values_from = value,
+    names_prefix = "year_"
+  ) %>%
+  mutate(change = year_2024 - year_1990)
+
+
+# visualise
+
+ggplot(lcm_change, aes(BNG_east, BNG_north, colour = change)) +
+  geom_point(size = 3) +
+  scale_colour_viridis_c() +
+  facet_grid(~landcover_type) +
+  coord_equal() +
+  theme_bw()
+
+# urban is the only landcover type to show any significant changes between 1990 and 2024
+
+
+
+
+# merge with BeeWalk data
+
+
+beewalk <- readRDS("./Data/beewalk_filtered.rds")
+
+# there are duplicates within the drivers dataset that need to be removed
+duplicates <- drivers[duplicated(drivers), ]
+#277
+drivers_unique <- drivers %>% distinct()
+
+# still lots of duplicated site, year combinations
+# something weird is going on with the dominant class names
+# row 8249 and 8551 have the exact same values in all colums but
+# lcm_2024_dominant_class_name  and lcm_1990_dominant_class_name
+# row 8249 has Suburban and 8551 has Improved_grassland in both
+# I am going to exclude these two columns as I am not using them anyway 
+# but worth asking Mariana how this happened
+
+drivers <- drivers %>%
+  select(-c(lcm_2024_dominant_class_name, lcm_1990_dominant_class_name))
+
+drivers_unique <- drivers %>% distinct()
+
+
+all_data <- beewalk %>%
+  left_join(drivers_unique, by = c("GridReference" = "site_id", "Year", "Precision"))
+
+
+saveRDS(all_data, "./Data/beewalk_drivers_merged.rds")
